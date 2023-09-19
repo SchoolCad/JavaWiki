2b03b03ef82e47f37d230feb975338c49c9cb99e
MatheusCamargoGinebro <matheuscginrbro@gmail.com>
2023-09-18 21:13:41 -0300

    noNet update

--- src/App.java
+++ src/App.java
@@ -1,27 +1,102 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
+        List<Category> list = new ArrayList<Category>();
+        Category auxCategory = new Category();
+        
         Scanner input = new Scanner(System.in);
+        String aux;
         int rUser; 
 
         do {
-            MenuBuilder.buildMenu("JavaWiki", "1. Categorias.;2. Ferramentas.;3. Sair.");
+            MenuBuilder.buildMenu("JavaWiki", "1. Ferramentas.;2. Categorias.;3. Sair.");
             rUser = Integer.parseInt(input.nextLine());
 
             switch (rUser) {
+                //Ferramentas
                 case 1:
-                    MenuBuilder.buildMenu("Ferramentas", "1. Gerenciar ferramentas.;2. Remover Ferramenta.;3. Listar Ferramentas.;4. ");
+                    do{
+                    MenuBuilder.buildMenu("Ferramentas", "1. Gerenciar ferramentas.;2. Remover Ferramenta.;3. Listar Ferramentas.;4. Editar Ferramenta;5. Voltar.");
                     rUser = Integer.parseInt(input.nextLine());
-                break;
+                        switch (rUser) {
+                        //Gerenciar
+                        case 1:
+                            
+                            break;
+                        
+                        //remover
+                        case 2:
+                            break;
+                        
+                        //Listar
+                        case 3:
+                            break;
+                        
+                        //Editar
+                        case 4:
+                            break;
+                        
+                         //Voltar
+                        case 5:
+                            System.out.println("Voltando...");
+                            break;
 
+                        default:
+                            System.out.println("opção invalida! Escolha entre 1 e 5.");
+                            break;
+                        }
+                    }while(rUser != 5);
+                    
+                break;
+                //Categorias
                 case 2:
+                    do{
+                    MenuBuilder.buildMenu("Categorias", "1. Gerenciar Categorias.;2. Remover Cateogrias.;3. Listar Categorias.;4. Editar Categorias;5. Voltar.");
+                    rUser = Integer.parseInt(input.nextLine());
+                        switch (rUser) {
+                        //Gerenciar
+                        case 1:
+
+                            System.out.print("Insira o nome da categoria: ");
+                            aux = input.nextLine();
+                            auxCategory.setUsage(aux);
+                            auxCategory.save();
+
+                            list.add(auxCategory);
+
+                            break;
+
+                        //remover
+                        case 2:                                
+                            break;
+                        
+                        //Listar
+                        case 3:
+                            break;
+                        
+                        //Editar
+                        case 4:
+                            break;
+                        
+                         //Voltar
+                        case 5:
+                            System.out.println("Voltando...");
+                            break;
+                        
+                        default:
+                            System.out.println("opção invalida! Escolha entre 1 e 5.");
+                            break;
+                        }
+                    }while(rUser != 5);
 
                 break;
-                
+                //Sair
                 case 3:
-
+                    System.out.println("Saindo...");
                 break;
             
                 default:
--- src/Category.java
+++ src/Category.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Category {
     private String usage;
 
@@ -9,14 +17,85 @@ public class Category {
         this.usage = usage;
     }
 
-    public Category() {}
+    public Category() {
+    }
 
     public Category(String usage) {
         this.usage = usage;
     }
-    
+
+    // Métodos
     @Override
     public String toString() {
         return "Category: " + usage;
     }
+
+    // Deletar categoria específica:
+    protected static Boolean delete(String target) {
+        Boolean boo = false;
+        try {
+            FileReader fileReader = new FileReader("../data/Categories.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                int b = 0;
+                if (!line.equals(target)) {
+
+                    try {
+                        PrintWriter writer = new PrintWriter(new FileWriter("../data/Categories.txt", (b == 1 ? true : false)));
+                        b = 1;
+                        writer.write(line + "\n");
+                        writer.close();
+                    } catch (IOException e) {
+                        System.out.println("Ocorreu um erro ao gravar o arquivo: " + e.getMessage());
+                    }
+
+                } else
+                    boo = true;
+                }
+
+            bufferedReader.close();
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+        return boo;
+    }
+
+    // Salvar o objeto em arquivo usando o trycath
+    protected Boolean save() {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("../data/categories.txt", true));
+            pw.println(this.usage);
+            pw.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar o arquivo: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Carregar toda a lista na memória em forma de lista:
+    protected List<Category> consult(String category) {
+        List<Category> list = new ArrayList<Category>();
+
+        try {
+            FileReader fileScan = new FileReader("../data/Categories.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileScan);
+            String linha;
+
+            while ((linha = bufferedReader.readLine()) != null) {
+                Category partFrom = new Category(linha);
+                list.add(partFrom);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("ERRO: Não foi possível ler do arquivo -> " + e.getMessage());
+        }
+
+        return list;
+    }
+
 }
