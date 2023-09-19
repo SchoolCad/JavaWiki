e9cab994bc2be9c349896fd1ecf45b44ddb24793
Vicente Santos <vidosantos45@gmail.com>
2023-09-18 22:45:14 -0300

    Update CRUDsys.java

--- src/CRUDsys.java
+++ src/CRUDsys.java
@@ -125,10 +125,6 @@ public class CRUDsys {
                         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
                     String[] newValues = newFileData.split(";");
                     for (int i = 0; i < newValues.length; i++) {
-                        if (newValues.length - 1 == i) {
-                            bufferedWriter.write(newValues[i]);
-                            break;
-                        }
                         bufferedWriter.write(newValues[i] + ";");
                         bufferedWriter.newLine();
                     }
